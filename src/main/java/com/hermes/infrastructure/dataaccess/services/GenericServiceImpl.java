76dab3a4b731ed11bcb3ca77ccbd6b5a1aed00f0
Ivan Azanpv <azanoviv02@gmail.com>
2016-11-05 23:40:20 +0300

    Enabling full Spring DI

--- src/main/java/com/hermes/infrastructure/dataaccess/services/GenericServiceImpl.java
+++ src/main/java/com/hermes/infrastructure/dataaccess/services/GenericServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
-public abstract class GenericServiceImpl<T extends AbstractPersistentObject> implements GenericService<T>{
+public class GenericServiceImpl<T extends AbstractPersistentObject> implements GenericService<T>{
 
     private final GenericRepository<T> repository;
 
